363f8ebdd1d2d72561cd3f68f8a2dcc3a63f0be4
Aaron Lucien <56093256+Aarou@users.noreply.github.com>
2020-03-27 17:42:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 function animate() {
- const animation = "animation-name: animation-one;"
- const duration = "animation-duration: 0.5s;"
+ document.addEventListener("DOMContentLoaded", () => {
+  const animation = "animation-name: animation-one;"
+  const duration = "animation-duration: 0.5s;"
  
- const buttonOne = document.getElementById("one")
- const buttonTwo = document.getElementById("two")
- const buttonThree = document.getElementById("three")
- const buttonFour = document.getElementById("four")
- const buttonFive = document.getElementById("five")
- const buttonSix = document.getElementById("six")
+  const buttonOne = document.getElementById("one")
+  const buttonTwo = document.getElementById("two")
+  const buttonThree = document.getElementById("three")
+  const buttonFour = document.getElementById("four")
+  const buttonFive = document.getElementById("five")
+  const buttonSix = document.getElementById("six")
  
- buttonOne.addEventListener("click", () => {
-  animation = "animation-name: animation-one; "
- })
- buttonTwo.addEventListener("click", () => {
-  animation = "animation-name: animation-two; "
- })
- buttonThree.addEventListener("click", () => {
-  animation = "animation-duration: 0.5s; "
- })
- buttonFour.addEventListener("click", () => {
-  animation = "animation-duration: 1s; "
- })
- buttonFive.addEventListener("click", () => {
-  animation = "animation-duration: 2s; "
- })
- buttonSix.addEventListener("click", () => {
-  animation = "animation-duration: 3s; "
- })  
+  buttonOne.addEventListener("click", () => {
+   animation = "animation-name: animation-one; "
+  })
+  buttonTwo.addEventListener("click", () => {
+   animation = "animation-name: animation-two; "
+  })
+  buttonThree.addEventListener("click", () => {
+   animation = "animation-duration: 0.5s; "
+  })
+  buttonFour.addEventListener("click", () => {
+   animation = "animation-duration: 1s; "
+  })
+  buttonFive.addEventListener("click", () => {
+   animation = "animation-duration: 2s; "
+  })
+  buttonSix.addEventListener("click", () => {
+   animation = "animation-duration: 3s; "
+  })  
  
- const image = document.getElementById("image")
+  const image = document.getElementById("image")
  
- image.style = animation + duration + "animation-iteration-count: infinite"
+  image.style = animation + duration + "animation-iteration-count: infinite"
+ 
+ })
 }
 
 animate()
