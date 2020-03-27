2ffdec9a9ce08be95aec465f7886ebdc85c575ff
Aaron Lucien <56093256+Aarou@users.noreply.github.com>
2020-03-27 16:37:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 function animate() {
- const animation = ""
- const duration = ""
+ const animation = "animation-name: animation-one;"
+ const duration = "animation-duration: 0.5s;"
+ 
+ const buttonOne = document.getElementById("one")
+ const buttonTwo = document.getElementById("two")
+ const buttonThree = document.getElementById("three")
+ const buttonFour = document.getElementById("four")
+ const buttonFive = document.getElementById("five")
+ const buttonSix = document.getElementById("six")
+ 
+ buttonOne.addEventListener("click", () => {
+  animation = "animation-name: animation-one;"
+ })
+ buttonTwo.addEventListener("click", () => {
+  animation = "animation-name: animation-two;"
+ })
+ buttonThree.addEventListener("click", () => {
+  animation = "animation-duration: 0.5s;"
+ })
+ buttonFour.addEventListener("click", () => {
+  animation = "animation-duration: 1s;"
+ })
+ buttonFive.addEventListener("click", () => {
+  animation = "animation-duration: 2s;"
+ })
+ buttonSix.addEventListener("click", () => {
+  animation = "animation-duration: 3s;"
+ }) 
 }
