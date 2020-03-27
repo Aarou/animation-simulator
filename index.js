4f14b92bead6cae726673aeb36948ea1102cc83c
Aaron Lucien <56093256+Aarou@users.noreply.github.com>
2020-03-27 19:06:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("one").addEventListener("click", () => {
+    document.getElementById("image").style.animationName = "animation-one"
+  })
+  document.getElementById("two").addEventListener("click", () => {
+    document.getElementById("image").style.animationName = "animation-two"
+  })
+  document.getElementById("three").addEventListener("click", () => {
+    document.getElementById("image").style.animationDuration = "0.5s"
+  })
+  document.getElementById("four").addEventListener("click", () => {
+    document.getElementById("image").style.animationDuration = "1s"
+  })
+  document.getElementById("five").addEventListener("click", () => {
+    document.getElementById("image").style.animationDuration = "2s"
+  })
+  document.getElementById("six").addEventListener("click" () => {
+    document.getElementById("image").style.animationDuration = "3s"
+  })
+})
